f811f47482f70051381ce70493f63c81a563d33b
gpanagiot <56256252+gpanagiot@users.noreply.github.com>
2022-01-19 19:21:43 +0200

    Add files via upload

--- FCFS.java
+++ FCFS.java
@@ -14,14 +14,13 @@ public class FCFS extends Scheduler {
 
         try {
             if (processes.get(0).GetBurstTime()==0){
-                System.out.println("Proccess finished FCFS");
+                //System.out.println("Proccess PID " + processes.get(0).getPCB().getPid() + " finished FCFS");
                 processes.get(0).getPCB().setState(ProcessState.TERMINATED,CPU.clock);
                 removeProcess(processes.get(0));
             }
             processes.get(0).SetBurstTime(processes.get(0).GetBurstTime()-1);
 
-            //reduce burst time
-            //System.out.println(processes.get(0).GetBurstTime());
+
 
             /* TODO: you need to add some code here
              * and change the return value */
--- SRTF.java
+++ SRTF.java
@@ -18,22 +18,18 @@ public class SRTF extends Scheduler {
             for (int i=0;i<processes.size();i++){
                 if(processes.get(i).GetBurstTime()==0){
                     processes.get(i).getPCB().setState(ProcessState.TERMINATED,CPU.clock);
-                    System.out.println("Proccess"+ (i+1) +" finished SRTF");    //number represents the relative number in the list of processes not the actual number of the process
+                    //System.out.println("Proccess"+ processes.get(i).getPCB().getPid() +" finished SRTF");
                     processes.remove(i);
                     i--;
                     continue;
                 }
-                /*if (i==0){
-                    index = 0;//processes.get(0).GetBurstTime();
-                }*/
+
                 if (processes.get(i).GetBurstTime() < processes.get(0).GetBurstTime()){
                     index = i;
                 }
             }
             processes.get(index).SetBurstTime(processes.get(index).GetBurstTime()-1);
 
-            //System.out.println(processes.get(index).GetBurstTime());
-            //reduce burst time -1
             /* TODO: you need to add some code here
              * and change the return value */
             return processes.get(index);
